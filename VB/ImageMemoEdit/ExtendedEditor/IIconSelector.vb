0b9bbb5e1697d79955b7254aa409904c7fa3eb13
DevExpressExampleBot <fakeEmail>
2023-05-17 19:18:00 +0400

    autogenerate VB

--- VB/ImageMemoEdit/ExtendedEditor/IIconSelector.vb
+++ VB/ImageMemoEdit/ExtendedEditor/IIconSelector.vb
@@ -1,11 +1,8 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Text
-
 Namespace ImageTextEdit
-	Public Interface IIconSelector
-		Event OnIconSelection As EditorDescendant.MyRepositoryItemMemoEdit.IconSelectionEventHandler
-	End Interface
 
+    Public Interface IIconSelector
+
+        Event OnIconSelection As EditorDescendant.MyRepositoryItemMemoEdit.IconSelectionEventHandler
+
+    End Interface
 End Namespace
