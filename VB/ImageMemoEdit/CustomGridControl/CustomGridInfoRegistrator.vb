0f00c1a0b65c4c81f142c5505f96770a807bd43d
DevExpressExampleBot <fakeEmail>
2023-05-19 17:20:32 +0400

    autogenerate VB

--- VB/ImageMemoEdit/CustomGridControl/CustomGridInfoRegistrator.vb
+++ VB/ImageMemoEdit/CustomGridControl/CustomGridInfoRegistrator.vb
@@ -1,27 +1,28 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Text
 Imports DevExpress.XtraGrid.Registrator
 Imports DevExpress.XtraGrid.Views.Base
 Imports DevExpress.XtraGrid
 
 Namespace ImageTextEdit
-	Public Class CustomGridInfoRegistrator
-		Inherits GridInfoRegistrator
-		Public Sub New()
-			MyBase.New()
-		End Sub
-		Public Overrides Function CreatePainter(ByVal view As BaseView) As BaseViewPainter
-			Return New CustomGridPainter(TryCast(view, CustomGridView))
-		End Function
-		Public Overrides ReadOnly Property ViewName() As String
-			Get
-				Return "CustomGridView"
-			End Get
-		End Property
-		Public Overrides Function CreateView(ByVal grid As GridControl) As BaseView
-			Return New CustomGridView(grid)
-		End Function
-	End Class
+
+    Public Class CustomGridInfoRegistrator
+        Inherits GridInfoRegistrator
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+
+        Public Overrides Function CreatePainter(ByVal view As BaseView) As BaseViewPainter
+            Return New CustomGridPainter(TryCast(view, CustomGridView))
+        End Function
+
+        Public Overrides ReadOnly Property ViewName As String
+            Get
+                Return "CustomGridView"
+            End Get
+        End Property
+
+        Public Overrides Function CreateView(ByVal grid As GridControl) As BaseView
+            Return New CustomGridView(grid)
+        End Function
+    End Class
 End Namespace
--- VB/ImageMemoEdit/ExtendedEditor/MyMemoEdit.vb
+++ VB/ImageMemoEdit/ExtendedEditor/MyMemoEdit.vb
@@ -1,36 +1,34 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports DevExpress.XtraEditors
 Imports System.ComponentModel
 
 Namespace EditorDescendant
-	<System.ComponentModel.DesignerCategory(""), System.ComponentModel.ToolboxItem(True)> _
-	Public Class MyMemoEdit
-		Inherits MemoEdit
-		#Region "Service"
-		Shared Sub New()
-			MyRepositoryItemMemoEdit.Register()
-		End Sub
 
-		Public Sub New()
-			MyBase.New()
-		End Sub
+    <System.ComponentModel.DesignerCategory("")>
+    <System.ComponentModel.ToolboxItem(True)>
+    Public Class MyMemoEdit
+        Inherits MemoEdit
 
-		Public Overrides ReadOnly Property EditorTypeName() As String
-			Get
-				Return MyRepositoryItemMemoEdit.EditorName
-			End Get
-		End Property
+'#Region "Service"
+        Shared Sub New()
+            MyRepositoryItemMemoEdit.Register()
+        End Sub
 
-		<DesignerSerializationVisibility(DesignerSerializationVisibility.Content)> _
-		Public Shadows ReadOnly Property Properties() As MyRepositoryItemMemoEdit
-			Get
-				Return TryCast(MyBase.Properties, MyRepositoryItemMemoEdit)
-			End Get
-		End Property
+        Public Sub New()
+            MyBase.New()
+        End Sub
 
-	 #End Region
-	End Class
+        Public Overrides ReadOnly Property EditorTypeName As String
+            Get
+                Return MyRepositoryItemMemoEdit.EditorName
+            End Get
+        End Property
+
+        <DesignerSerializationVisibility(DesignerSerializationVisibility.Content)>
+        Public Overloads ReadOnly Property Properties As MyRepositoryItemMemoEdit
+            Get
+                Return TryCast(MyBase.Properties, MyRepositoryItemMemoEdit)
+            End Get
+        End Property
+'#End Region
+    End Class
 End Namespace
